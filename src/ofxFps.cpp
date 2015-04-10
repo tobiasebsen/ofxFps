9a7b0e7c11c23395d8b76308acef4bab367f21b6
tobiasebsen <tobiasebsen@gmail.com>
2015-04-10 15:53:43 +0200

    Added ticks and history

--- src/ofxFps.cpp
+++ src/ofxFps.cpp
@@ -1,7 +1,23 @@
 #include "ofxFps.h"
 
+struct less_first : std::binary_function<Tick,Tick,bool>
+{
+    inline bool operator()( const Tick& lhs, const Tick& rhs )
+    {
+        return lhs.second < rhs.second;
+    }
+};
+
 void ofxFps::begin() {
 	timeBegin = ofGetElapsedTimeMicros();
+    map<string, unsigned long long>::iterator it = ticks.begin();
+    for (; it!=ticks.begin(); ++it) {
+        it->second = timeBegin;
+    }
+}
+
+void ofxFps::tick(string name) {
+    ticks[name] = ofGetElapsedTimeMicros();
 }
 
 void ofxFps::end() {
@@ -9,6 +25,10 @@ void ofxFps::end() {
 	timeFrame = now - timeEnd;
 	timeUpdate = now - timeBegin;
 	timeEnd = now;
+    
+    ticks_sorted.clear();
+    ticks_sorted.assign(ticks.begin(), ticks.end());
+    sort(ticks_sorted.begin(), ticks_sorted.end(), less_first());
 }
 
 float ofxFps::getFps() {
@@ -17,4 +37,91 @@ float ofxFps::getFps() {
 
 float ofxFps::getLoad() {
 	return ((float)timeUpdate / (float)timeFrame);
+}
+
+string ofxFps::toString(int fpsPrecision, int loadPrecision, bool useTicks) {
+    stringstream ss;
+    ss << ofToString(getFps(), 1);
+    ss << " fps (";
+
+    if (useTicks && ticks.size() > 0) {
+        vector<Tick>::iterator it=ticks_sorted.begin();
+        unsigned long long previous = timeBegin;
+        for (; it!=ticks_sorted.end(); ) {
+            ss << it->first;
+            ss << ": ";
+            if (it->second > previous)
+                ss << ofToString(100. * (float)(it->second - previous) / (float)timeFrame, loadPrecision);
+            else
+                ss << "0";
+            ss << " %";
+            previous = it->second;
+            if (++it != ticks_sorted.end())
+                ss << ", ";
+        }
+        ss << ")";
+    }
+    else {
+        ss << ofToString(getLoad()*100.f, 0);
+        ss << " %)";
+    }
+    return ss.str();
+}
+
+void ofxFps::draw(int x, int y) {
+    if (getFps() < 20.f || getLoad() > 0.9f)
+        ofSetColor(255, 0, 0);
+    else
+        ofSetColor(255);
+    ofDrawBitmapString(toString(), x, y);
+}
+
+void ofxFps::draw(int x, int y, string label, bool drawTicks) {
+
+    if (getFps() < 15.f || getLoad() > 0.9f)
+        ofSetColor(255, 0, 0);
+    else
+        ofSetColor(255);
+
+    ofDrawBitmapString(label + ": " + toString(1, 0, drawTicks), x, y);
+}
+
+ofxFpsHistory::ofxFpsHistory(int size, bool autoMax) {
+    setSize(size);
+    this->autoMax = autoMax;
+}
+
+void ofxFpsHistory::setSize(int size) {
+    this->size = size;
+}
+
+void ofxFpsHistory::setMax(float max) {
+    this->max = max;
+}
+
+void ofxFpsHistory::setAutoMax(bool autoMax) {
+    this->autoMax = autoMax;
+}
+
+void ofxFpsHistory::add(float f) {
+    history.push_front(f);
+    while (history.size() > size)
+        history.pop_back();
+}
+
+void ofxFpsHistory::draw(float x, float y, float height) {
+
+    if (autoMax) {
+        max = 0.f;
+        list<float>::iterator it=history.begin();
+        for (; it!=history.end(); ++it) {
+            if (*it > max)
+                max = *it;
+        }
+    }
+    list<float>::iterator it=history.begin();
+    for (; it!=history.end(); ++it) {
+        ofLine(x, y+height, x, y+height-(*it/max)*height);
+        x++;
+    }
 }
--- src/ofxFps.h
+++ src/ofxFps.h
@@ -2,18 +2,50 @@
 
 #include "ofMain.h"
 
+typedef std::pair<string, unsigned long long> Tick;
+
+// FRAME RATE
 class ofxFps {
 public:
 
 	void begin();
+    void tick(string name);
 	void end();
 
 	float getFps();
 	float getLoad();
+    
+    string toString(int fpsPrecision = 1, int loadPrecision = 0, bool useTicks = true);
+    
+    void draw(int x, int y);
+    void draw(int x, int y, string label, bool drawTicks = true);
 
 private:
 	unsigned long long timeBegin;
 	unsigned long long timeEnd;
 	unsigned int timeFrame;
 	unsigned int timeUpdate;
+    
+    map<string, unsigned long long> ticks;
+    vector<Tick> ticks_sorted;
+};
+
+// HISTORY
+class ofxFpsHistory {
+public:
+    
+    ofxFpsHistory(int size = 100, bool autoMax = true);
+    
+    void setSize(int size);
+    void setMax(float max);
+    void setAutoMax(bool autoMax);
+    
+    void add(float f);
+    void draw(float x, float y, float height);
+
+private:
+    int size;
+    list<float> history;
+    bool autoMax;
+    float max;
 };
